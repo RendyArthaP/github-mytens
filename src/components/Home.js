b15b5e062884a0a79c3acb6a2e07192eb89e31f2
Rendy Artha <rendyartha312@gmail.com>
2021-04-29 16:52:46 +0700

    components home

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getGithubProfile } from '../redux/actions/githubprofile.actions';
+import { getGithubRepository } from '../redux/actions/githubrepository.actions';
+import GithubProfile from './GithubProfile';
+import GithubRepository from './GithubRepository';
+import NavbarMenu from './NavbarMenu';
+
+const Home = () => {
+  const dispatch = useDispatch()
+  const dataGithubProfile = useSelector((state) => state.handleGetGithubProfile.data)
+  const dataGithubRepository = useSelector((state) => state.handleGetGithubRepository.data)
+
+  useEffect(() => {
+    dispatch(getGithubProfile())
+    dispatch(getGithubRepository())
+  }, [dispatch])
+
+  return (
+    <div>
+      <NavbarMenu />
+      <GithubProfile dataGithubProfile = {dataGithubProfile} />
+      <GithubRepository dataGithubRepository = {dataGithubRepository} />
+    </div>
+  )
+}
+
+export default Home
